08c892cc8551513e319755c619eb17e081766390
Bryson Nelson <brysonnelson@Brysons-MacBook-Pro.local>
2018-11-20 21:19:25 -0800

    user model

---
+++ models/user.js
@@ -0,0 +1,34 @@
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [6]
+      }
+    }
+  });
+  return User;
+};
