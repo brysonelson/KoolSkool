db995e0680db0ee1d66c06684d3466ae17255f39
SueJStevens <StevensStock@gmail.com>
2018-11-21 20:48:52 -0800

    Created Models to reflect Schema Design

---
+++ models/parent_child_map.js
@@ -0,0 +1,70 @@
+module.exports = function(sequelize, DataTypes) {
+  var ParentChild = sequelize.define(
+    "parent_child_map",
+    {
+      /*************************************************************************/
+      /* Relationship Definition and Usage Example:
+      /  Describes the relationship between the parent record and the student; 
+      /  Example:  Mother, Father, Stepparent, Grandparent.
+      /*************************************************************************/
+      relationship: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1, 35]
+        }
+      },
+      /*************************************************************************/
+      /* Contact Sequence Definition and Usage Example:
+      /  This field defines the sequence that parents or guardians would be 
+      /  called in the case of an emergency.
+      /*************************************************************************/
+      contact_sequence: {
+        type: DataTypes.TINYINT,
+        allowNull: true,
+        validate: {
+          isInt: true
+        }
+      }
+    },
+    {
+      /*************************************************************************/
+      /* Table Configuration Options:
+      /*
+      /* don't delete database entries but set the newly added attribute 
+      /* deletedAt to the current date (when deletion was done). paranoid will 
+      /* only work if timestamps are enabled
+      /* paranoid: true,
+      /* 
+      /* define the table's name
+      /* tableName: 'my_very_custom_table_name',
+      /* 
+      /*************************************************************************/
+
+      /*************************************************************************/
+      /* disable the modification of table names; By default, sequelize will 
+      /* automatically transform all passed model names (first parameter of 
+      /* define) into plural. if you don't want that, set the following
+
+      /* don't use camelcase for automatically added attributes but underscore
+      /  style so updatedAt will be updated_at
+      /*************************************************************************/
+      freezeTableName: true,
+      underscored: true
+    }
+  );
+
+  ParentChild.associate = function(models) {
+    models.Parents.belongsToMany(models.Students, {
+      as: "Parents",
+      through: "parent_child_map",
+      foreignKey: "student_id"
+    });
+    models.Students.belongsToMany(models.Parents, {
+      as: "Children",
+      through: "parent_child_map",
+      foreignKey: "parent_id"
+    });
+  };
+  return ParentChild;
+};
---
+++ models/student_course_map.js
@@ -0,0 +1,51 @@
+
+module.exports = function(sequelize, DataTypes) {
+  var StudentCourse = sequelize.define(
+    "student_course_map",
+    {
+      status: {
+        type: DataTypes.STRING,
+        allowNull: true
+      }
+    },
+    {
+      /*************************************************************************/
+      /* Table Configuration Options:
+      /*
+      /* don't delete database entries but set the newly added attribute 
+      /* deletedAt to the current date (when deletion was done). paranoid will 
+      /* only work if timestamps are enabled
+      /* paranoid: true,
+      /* 
+      /* define the table's name
+      /* tableName: 'my_very_custom_table_name',
+      /* 
+      /*************************************************************************/
+
+      /*************************************************************************/
+      /* disable the modification of table names; By default, sequelize will 
+      /* automatically transform all passed model names (first parameter of 
+      /* define) into plural. if you don't want that, set the following
+
+      /* don't use camelcase for automatically added attributes but underscore
+      /  style so updatedAt will be updated_at
+      /*************************************************************************/
+      freezeTableName: true,
+      underscored: true
+    }
+  );
+
+  StudentCourse.associate = function(models) {
+    models.Students.belongsToMany(models.Course, {
+      as: "Students",
+      through: "student_course_map",
+      foreignKey: "course_id"
+    });
+    models.Course.belongsToMany(models.Students, {
+      as: "Course_Enrolled",
+      through: "student_course_map",
+      foreignKey: "student_id"
+    });
+  };
+  return StudentCourse;
+};
