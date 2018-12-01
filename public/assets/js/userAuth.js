606495e5c079978891f44f0bbe81c4cc4c3b35f0
Bryson Nelson <brysonnelson@Brysons-MacBook-Pro.local>
2018-12-01 13:21:55 -0800

    redirect routes based on users role, and initial personnel id storage into user table

--- public/assets/js/userAuth.js
+++ public/assets/js/userAuth.js
@@ -1,7 +1,26 @@
-$(Document).ready(function() {
+$(document).ready(function() {
   $("#user-logout").click(function() {
     $.get("/logout", function() {
       console.log("logged out!");
+    });
+  });
+
+  $(".form-login").on("submit", function(event) {
+    event.preventDefault();
+    $.post("/login", {
+      email: $("#email").val(),
+      password: $("#password").val()
+    }).then(function(data) {
+      window.location.replace(data.url);
+    });
+  });
+
+  $("#signup-auth").change(function() {
+    var authMode = $("#signup-auth").val();
+    console.log("test");
+    $.get("/api/personnel", function(req, res) {
+      console.log("===============REQ: " + req);
+      console.log("===============RES: " + res);
     });
   });
 });
