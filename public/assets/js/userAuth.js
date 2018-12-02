7690540c90f80ff4fc30f083ab21dbb651f087a6
Bryson Nelson <brysonnelson@Brysons-MBP.attlocal.net>
2018-12-01 16:28:24 -0800

    finished all auth levels, also storing personel_id in the users table

--- public/assets/js/userAuth.js
+++ public/assets/js/userAuth.js
@@ -19,8 +19,30 @@ $(document).ready(function() {
     var authMode = $("#signup-auth").val();
     console.log("test");
     $.get("/api/personnel", function(req, res) {
-      console.log("===============REQ: " + req);
-      console.log("===============RES: " + res);
+    }).then(function(personnel) {
+      console.log("personnel " + personnel);
+
+      for (var i = 0; i < personnel.length; i++) {
+        console.log(personnel[i].first_name);
+        console.log(personnel[i]);
+
+        var personnelSelector = $("#personnel_select");
+
+        // var option = $("<li><a role='option' class='dropdown-item' aria-disabled='false' tabindex='0' aria-selected='false'><span class='text>" + personnel[i].first_name + "</span></a></li>")
+        // .appendTo(personnelSelector);
+        var option = $("<option>");
+        option.attr("value", personnel[i].id);
+        option.text(
+          "ID: " +
+            personnel[i].id +
+            " - " +
+            personnel[i].first_name +
+            " " +
+            personnel[i].last_name
+        );
+        option.appendTo(personnelSelector);
+        $(".selectpicker").selectpicker("refresh");
+      }
     });
   });
 });
