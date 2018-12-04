80764ac0ae5f4c1fec97776576d0251bf4932815
Bryson Nelson <brysonnelson@Brysons-MBP.attlocal.net>
2018-12-03 17:06:24 -0800

    change password functionality added to nav bar

--- public/assets/js/userAuth.js
+++ public/assets/js/userAuth.js
@@ -1,33 +1,41 @@
+//once the document is loaded
 $(document).ready(function() {
+  //when you click the logout button
   $("#user-logout").click(function() {
+    //get the logout page
     $.get("/logout", function() {
+      //let the user know if there are errors
       if (err) {
         throw err;
       }
     });
   });
 
+  //when the login form is submitted
   $("#login_form").on("submit", function(event) {
+    //prevent the default
     event.preventDefault();
+    //post the request to the login route
     $.post("/login", {
       email: $("#email").val(),
       password: $("#password").val()
     }).then(function(data) {
+      //then load the relevant page the user should be sent to
       window.location.replace(data.url);
     });
   });
 
+  //when the signup auth drop down changes on the sign up page
   $("#signup-auth").change(function() {
+      //store the users auth mode
     // eslint-disable-next-line no-unused-vars
     var authMode = $("#signup-auth").val();
-    console.log("test");
     // eslint-disable-next-line no-unused-vars
     $.get("/api/personnel", function(req, res) {
       if (err) {
         throw err;
       }
     }).then(function(personnel) {
-      console.log("personnel " + personnel);
 
       for (var i = 0; i < personnel.length; i++) {
         console.log(personnel[i].first_name);
@@ -62,6 +70,7 @@ $("#reset-email-btn").click(function() {
   var email = $("#forgot-email").val();
 
   $.post("/forgot", { email: email }).then(function() {
+    console.log("Logged in");
     alert("sent!");
   });
 });
