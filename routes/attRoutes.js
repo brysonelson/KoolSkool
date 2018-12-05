d984d1f375aa0abae1189fd54066e9ddd8453f32
SueJStevens <StevensStock@gmail.com>
2018-12-04 19:20:58 -0800

    Changed Readme and Style Emergency

--- routes/attRoutes.js
+++ routes/attRoutes.js
@@ -1,19 +1,20 @@
 //attendence files
 var db = require("../models");
-module.exports = function(app) {
-  app.get("/att", function(req, res) {
+var moment = require("moment");
+module.exports = function (app) {
+  app.get("/att", function (req, res) {
     res.render("att", {
       nav: true
     });
   });
-  app.get("/photo-attendence", function(req, res) {
+  app.get("/photo-attendence", function (req, res) {
     res.render("photo-attendence", {
       nav: true
     });
   });
 
   //USING THIS TO RENDER COURSE NAMES ONTO THE FILE
-  app.get("/teachers/attendence", function(req, res) {
+  app.get("/teachers/attendence", function (req, res) {
     db.Course.findAll({
       include: [
         {
@@ -23,7 +24,7 @@ module.exports = function(app) {
           foreignKey: "student_id"
         }
       ]
-    }).then(function(dbCourse) {
+    }).then(function (dbCourse) {
       res.render("attendence", {
         nav: true,
         course: dbCourse
@@ -32,9 +33,43 @@ module.exports = function(app) {
   });
 
   // Create the bulk records in the attendence api
-  app.post("/teachers/api/attendence", function(req, res) {
-    db.Attendence.bulkCreate(req.body).then(function(dbattendence) {
+  app.post("/teachers/api/attendance", function (req, res) {
+    db.Attendence.bulkCreate(req.body).then(function (dbattendence) {
       res.json(dbattendence);
     });
+  });
+
+  var $today = moment().format("YYYY-MM-DD");
+  $today = '2018-12-02';
+
+  app.get("/api/viewAttendance1", function (req, res) {
+    db.sequelize
+      .query(
+        "Select DISTINCT c.course_descr, c.id, s.first_name, s.last_name, a.absent from courses c inner join student_course_map m on c.id = m.course_id inner join students s on m.student_id = s.id inner join attendence a on m.student_id = a.student_id and m.course_id = a.course_id Where a.attendence_dt = '" + $today + "'",
+        {
+          type: db.sequelize.QueryTypes.SELECT
+        }
+      )
+      .then(function (result) {
+        res.json(result);
+      });
+  });
+
+  app.get("/teachers/reattendence", function (req, res) {
+    db.sequelize
+      .query(
+        "Select DISTINCT c.course_descr, c.course_id, s.first_name, s.last_name, a.absent from courses c inner join student_course_map m on c.id = m.course_id inner join students s on m.student_id = s.id inner join attendence a on m.student_id = a.student_id and m.course_id = a.course_id Where a.attendence_dt = '" +
+        $today +
+        "'",
+        {
+          type: db.sequelize.QueryTypes.SELECT
+        }
+      )
+      .then(function (result) {
+        res.render("reattendence", {
+          nav: true,
+          reattendence: result
+        });
+      });
   });
 }; //end module.exports
